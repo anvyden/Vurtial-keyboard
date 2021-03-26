ac6164e122f9b64ad47451f79c28a26362730ed4
anvyden <anvyden@gmail.com>
2021-03-26 14:53:49 +0700

    create keyboard.js

---
+++ keyboard/Keyboard.js
@@ -0,0 +1,72 @@
+const Keyboard = {
+  elements: {
+    main: null,
+    keysContainer: null,
+    keys: []
+  },
+
+  eventHandlers: {
+    oninput: null,
+    onclose: null
+  },
+
+  properties: {
+    value: "",
+    capsLock: false
+  },
+
+  init() {
+    // Create main elements
+    this.elements.main = document.createElement('div');
+    this.elements.keysContainer = document.createElement('div');
+
+    // Setup main elements
+    this.elements.main.classList.add('keyboard', '1keyboard--hidden');
+    this.elements.keysContainer.classList.add('keyboard__keys');
+
+    //add to DOM
+    this.elements.main.appendChild(this.elements.keysContainer);
+    document.body.appendChild(this.elements.main);
+  },
+
+  _createKeys() {
+    const fragment = document.createDocumentFragment();
+    const keyLayout = [
+      '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'backspace',
+      'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
+      'caps', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'enter',
+      'done', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '?',
+      'space'
+    ];
+
+    // creates HTML for an icon
+    const createIconHTML = (icon_name) => {
+      return `<i class='material-icons'>${icon_name}</i>`
+    };
+
+    keyLayout.forEach(key => {
+      const keyElement = document.createElement('button');
+      const insertLineBreak = ['backspace', 'p', 'enter', '?'].indexOf(key) !== 1;
+    });
+  },
+
+  _triggerEvent(handlerName) {
+    console.log('Event Triggered! Event Name: ' + handlerName);
+  },
+
+  _toggleCapsLock() {
+    console.log('Caps Lock Toggled!');
+  },
+
+  open(initialValue, oninput, onclose) {
+
+  },
+
+  close() {
+
+  }
+};
+
+window.addEventListener("DOMContentLoaded", function() {
+  keyboard.init();
+});
